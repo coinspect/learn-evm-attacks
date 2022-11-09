3669842a869d8138b00b1223a80048f451329e35
Nine December <108370058+nine-december@users.noreply.github.com>
2022-11-09 11:09:48 -0300

    feat: added file template

--- src/Token.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
-
-import {ERC20} from "solmate/tokens/ERC20.sol";
-
-contract Token is ERC20("Token", "TOK", 18) {
-
-}
---
+++ test/Business_Logic/Bvaults.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "forge-std/Test.sol";
+import {CheatCodes} from "../interfaces/00_CheatCodes.interface.sol";
+
+// forge test --match-contract Exploit_PROTOCOL_NAME -vvv
+/*
+On DATE an attacker stolen AMOUNT in TYPEW tokens from an PROTOCOL.
+
+
+// Attack Overview
+Total Lost: 
+Attack Tx: 
+Ethereum Transaction Viewer: 
+
+Exploited Contract: 
+Attacker Address: 
+Attacker Contract: 
+Attack Block:  
+
+// Key Info Sources
+Twitter: 
+Writeup: 
+Article: 
+Code: 
+
+
+Principle: VULN PRINCIPLE
+
+
+ATTACK:
+
+
+MITIGATIONS:
+1)
+
+*/
