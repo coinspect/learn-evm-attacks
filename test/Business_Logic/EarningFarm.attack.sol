95b4a450159607080c14baf701ee30e9a6cf680c
Nine December <108370058+nine-december@users.noreply.github.com>
2022-11-29 13:52:26 -0300

    Update EarningFarm.attack.sol

--- test/Business_Logic/EarningFarm.attack.sol
+++ test/Business_Logic/EarningFarm.attack.sol
@@ -8,36 +8,75 @@ import {IERC20} from "../interfaces/IERC20.sol";
 import {IWETH9} from '../interfaces/IWETH9.sol';
 import {BalancerFlashloan} from "../utils/BalancerFlashloan.sol";
 
-// forge test --match-contract Exploit_PROTOCOL_NAME -vvv
+// forge test --match-contract Exploit_EarningFarm -vvv
 /*
-On DATE an attacker stole AMOUNT in TYPE tokens from an PROTOCOL.
+On Oct 14, 2022 an attacker stole 200 ETH from EarningFarm.
 
 
 // Attack Overview
-Total Lost: 
+Total Lost: ~750ETH (550 ETH were returned by a MEV frontrunning bot, 200 ETH were stolen)
 Attack Tx: https://etherscan.io/tx/0x160c5950a01b88953648ba90ec0a29b0c5383e055d35a7835d905c53a3dda01e
-Ethereum Transaction Viewer: 
+Ethereum Transaction Viewer: https://tx.eth.samczsun.com/ethereum/0x160c5950a01b88953648ba90ec0a29b0c5383e055d35a7835d905c53a3dda01e
 
-Exploited Contract: 
-Attacker Address: 
-Attacker Contract: 
+Exploited Contract: https://etherscan.io/address/0xe39fd820b58f83205db1d9225f28105971c3d309
+Attacker Address: 0xdf31f4c8dc9548eb4c416af26dc396a25fde4d5f
+Attacker Contract: https://etherscan.io/address/0x983dfBa1c0724786598Af0E63a9a6f94aAbd24A1
 Attack Block: 15746342 
 
 // Key Info Sources
 Twitter: https://twitter.com/Supremacy_CA/status/1581012823701786624
-Writeup: 
-Article: 
-Code: 
+Code: https://etherscan.io/address/0xe39fd820b58f83205db1d9225f28105971c3d309#code
 
 
-Principle: VULN PRINCIPLE
+Principle: Unchecked Flashloan reception
 
+    function receiveFlashLoan(
+        IERC20[] memory tokens,
+        uint256[] memory amounts,
+        uint256[] memory feeAmounts,
+        bytes memory userData
+    ) public payable {
+        require(msg.sender == balancer, "only flashloan vault"); --------- CHECKS ONLY THE ORIGIN OF THE LOAN
+        // ------------------------------------------------------ SHOULD CHECK ALSO THAT WAS REQUESTED BY EF!
+        uint256 loan_amount = amounts[0];
+        uint256 fee_amount = feeAmounts[0];
+
+        if (keccak256(userData) == keccak256("0x1")){
+          _deposit(loan_amount, fee_amount);
+        }
+        if (keccak256(userData) == keccak256("0x2")){
+          _withdraw(loan_amount, fee_amount);
+        }
+    }
+
+    function _withdraw(uint256 amount, uint256 fee_amount) internal {
+        uint256 steth_amount = amount.safeMul(IERC20(asteth).balanceOf(address(this))).safeDiv(getDebt());
+        if (IERC20(weth).allowance(address(this), aave) != 0) {IERC20(weth).safeApprove(aave, 0);}
+        IERC20(weth).safeApprove(aave, amount);
+
+        IAAVE(aave).repay(weth, amount, 2, address(this));
+        IAAVE(aave).withdraw(lido, steth_amount, address(this));
+
+        if (IERC20(lido).allowance(address(this), curve_pool) != 0) {IERC20(lido).safeApprove(curve_pool, 0);}
+        IERC20(lido).safeApprove(curve_pool, steth_amount);
+        ICurve(curve_pool).exchange(1, 0, steth_amount, 0);
+
+        (bool status, ) = weth.call.value(amount.safeAdd(fee_amount))("");
+        require(status, "transfer eth failed");
+        IERC20(weth).safeTransfer(balancer, amount.safeAdd(fee_amount));
+    }
+
+VULNERABILITY
+1) The receiveFlashloan function does not checks if the loan was requested by the contract. It is worth noting that Balancer
+flashloan function does not include the initiator in the function signature.
 
 ATTACK:
-1)
+1) Desposit a tiny amount
+2) Request a flashloan from balancer to EFVault
+3) Withdraw the amount backed by the leftover
 
 MITIGATIONS:
-1)
+1) Implement flashloans that are EIP-3156 compliant in terms of passing the msg.sender as a parameter of the flashloan callback.
 
 */
 interface IDVM {
@@ -75,6 +114,7 @@ contract Exploit_EarningFarm is TestHarness, TokenBalanceTracker, BalancerFlashl
     function DVMFlashLoanCall(address arg0, uint256 arg1, uint256 arg2, bytes memory) external {
         require(msg.sender == address(dvm), 'Only DVM');
         require(arg0 == address(this), 'Only requested by this');
+
         uint256 wethAmt = weth.balanceOf(address(this));
         uint256 ethBefore = address(this).balance;
 
