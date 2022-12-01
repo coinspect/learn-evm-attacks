2ed36d6e36857fae764b06b78926cd5266931dbb
Nine December <108370058+nine-december@users.noreply.github.com>
2022-12-01 11:35:13 -0300

    feat: imports fix Olympus

--- test/Bad_Data_Validation/Bond_OlympusDAO/Bond_OlympusDAO.attack.sol
+++ test/Bad_Data_Validation/Bond_OlympusDAO/Bond_OlympusDAO.attack.sol
@@ -2,10 +2,10 @@
 pragma solidity ^0.8.17;
 
 import "forge-std/Test.sol";
-import {TestHarness} from "../TestHarness.sol";
-import {IERC20} from "../interfaces/IERC20.sol";
+import {TestHarness} from "../../TestHarness.sol";
+import {IERC20} from "../../interfaces/IERC20.sol";
 
-import {TokenBalanceTracker} from '../modules/TokenBalanceTracker.sol';
+import {TokenBalanceTracker} from '../../modules/TokenBalanceTracker.sol';
 
 // forge test --match-contract Exploit_OlympusDao -vvv
 
