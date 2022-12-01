e3e99648cd924f756dc7bf108739dd43ccc268ca
Nine December <108370058+nine-december@users.noreply.github.com>
2022-12-01 11:35:53 -0300

    feat: imports fix Superfluid

--- test/Bad_Data_Validation/Superfluid/Superfluid.attack.sol
+++ test/Bad_Data_Validation/Superfluid/Superfluid.attack.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.17;
 
 import "forge-std/Test.sol";
-import {TestHarness} from "../TestHarness.sol";
-import {TokenBalanceTracker} from '../modules/TokenBalanceTracker.sol';
-import {IERC20} from "../interfaces/IERC20.sol";
+import {TestHarness} from "../../TestHarness.sol";
+import {TokenBalanceTracker} from '../../modules/TokenBalanceTracker.sol';
+import {IERC20} from "../../interfaces/IERC20.sol";
 
 
 // forge test --match-contract Exploit_Superfluid -vvv
