4ea8ae57981bf2e8aa2de2101a4e4725378cf1bb
Nine December <108370058+nine-december@users.noreply.github.com>
2022-12-01 11:38:57 -0300

    feat: imports fix Bridges

--- test/Bridges/RoninBridge/RoninBridge.attack.sol
+++ test/Bridges/RoninBridge/RoninBridge.attack.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.17;
 
 import "forge-std/Test.sol";
-import {TestHarness} from "../TestHarness.sol";
-import {TokenBalanceTracker} from '../modules/TokenBalanceTracker.sol';
+import {TestHarness} from "../../TestHarness.sol";
+import {TokenBalanceTracker} from '../../modules/TokenBalanceTracker.sol';
 
 // forge test --match-contract Exploit_RoninBridge -vvv
 /*
