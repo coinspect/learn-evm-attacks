c4cc202557ef51e9eaaa2a8a9acbca01d73f9170
Nine December <108370058+nine-december@users.noreply.github.com>
2022-12-06 16:18:48 -0300

    feat: added DFX Finance graphs and README

--- test/Reentrancy/DFXFinance/DFXFinance.attack.sol
+++ test/Reentrancy/DFXFinance/DFXFinance.attack.sol
@@ -6,55 +6,6 @@ import {TestHarness} from "../../TestHarness.sol";
 import {IERC20} from '../../interfaces/IERC20.sol';
 import {IUniswapV3Pair} from '../../utils/IUniswapV3Pair.sol';
 
-// forge test --match-contract Exploit_DFXFinance -vvv
-/*
-On Nov 10, 2022 an attacker stole more than 6MM USD in various tokens and transactions from DFX Finance.
-The attacker requested a flashloan which allowed a backdoor deposit and withdraw. Then, the attacker
-reproduced this path several times.
-
-// Attack Overview
-Total Lost: ~6MM USD
-First Attack Tx: https://etherscan.io/tx/0x390def749b71f516d8bf4329a4cb07bb3568a3627c25e607556621182a17f1f9
-Subsequent Attack Tx: https://etherscan.io/tx/0x6bfd9e286e37061ed279e4f139fbc03c8bd707a2cdd15f7260549052cbba79b7
-Ethereum Transaction Viewer: https://tx.eth.samczsun.com/ethereum/0x6bfd9e286e37061ed279e4f139fbc03c8bd707a2cdd15f7260549052cbba79b7
-
-Exploited Contract: https://etherscan.io/address/0x46161158b1947D9149E066d6d31AF1283b2d377C#code
-Attacker Address: https://etherscan.io/address/0x14c19962e4a899f29b3dd9ff52ebfb5e4cb9a067
-Attacker Contract: https://etherscan.io/address/0x6cfa86a352339e766ff1ca119c8c40824f41f22d
-Attack Block:  15941674
-
-// Key Info Sources
-Twitter: https://twitter.com/peckshield/status/1590831589004816384, https://twitter.com/DFXFinance/status/1590858722728972289
-
-
-Principle: Side Reentrance (Backdoor)
-
-    function flash(
-        address recipient,
-        uint256 amount0,
-        uint256 amount1,
-        bytes calldata data
-    ) external transactable noDelegateCall isNotEmergency {
-        ...
-        ...
-        uint256 balance0After = IERC20(derivatives[0]).balanceOf(address(this));
-        uint256 balance1After = IERC20(derivatives[1]).balanceOf(address(this));
-
-        require(balance0Before.add(fee0) <= balance0After, 'Curve/insufficient-token0-returned');
-        require(balance1Before.add(fee1) <= balance1After, 'Curve/insufficient-token1-returned');
-    }
-
-ATTACK:
-Because there is no reentrancy protection in the flashloan function and token balances are checked to determine if the loan was paid back,
-the attacker simply asked for a loan and deposited the amount requested to mint shares inside the loan callback. 
-Because the balance checked after executing the flashloan callback was satisfied, the loan succeded.
-Then, the attacker simply called withdraw and stole the loan amount.
-
-MITIGATIONS:
-1) Use reentrancy protection for flashloans
-2) Check if the flashloan key variables could be manipuladed by side-entering the contract and if so, its impact.
-*/
-
 interface IDFX {
     function flash(
         address recipient,
